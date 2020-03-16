fac61f989b381386bad2c3f7a1bc8ceb1449f866
Dalyna Pak <dalyna.pak@hotmail.fr>
2020-03-16 15:12:27 -0400

    Create window
    Signed-off-by: Dalyna Pak <dalyna.pak@hotmail.fr>

---
+++ OpenGL/OpenGL/main.cpp
@@ -0,0 +1,42 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <stdio.h>
+
+int main(void) {
+	
+	GLFWwindow *window;
+
+	// start GL context and O/S window using the GLFW helper library
+	if (!glfwInit()) 
+	{
+		fprintf(stderr, "ERROR: could not start GLFW3\n");
+		return -1;
+	}
+
+	// create a windowed mode and its OpenGL Context
+	window = glfwCreateWindow(640, 480, "OpenGL Project", NULL, NULL);
+
+	if (!window)
+	{
+		fprintf(stderr, "ERROR: could not open window with GLFW3\n");
+		glfwTerminate();
+		return -1;
+	}
+
+	// make the window's context current
+
+	//loop until the user close the window
+	while (!glfwWindowShouldClose(window))
+	{
+		glClear(GL_COLOR_BUFFER_BIT);
+		// render the OpenGL here
+
+		// sweap front and back buffers
+		glfwSwapBuffers(window);
+
+		// poll for and process envets
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+}
